0c6b6e2953b525c3b84d9e5522ef8f0b3575c992
AkumuOmolo <akumu.omoloh@gmail.com>
2025-07-27 10:53:13 +0300

    Adds code along

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-// Your code here
+// import { currentAgeForBirthYear } from "../utils";
+
+
+export function currentAgeForBirthYear(birthYear) {
+  return new Date().getFullYear() - birthYear;
+}
+
+
+
+// describe("currentAgeForBirthYear", () => {
+//   it("returns the age of a person based on the year of birth", () => {
+//     const birthYear = 1984;
+//     const ageOfPerson = currentAgeForBirthYear(birthYear);
+//     expect(ageOfPerson).toBe(38);
+//   });
+// })
+
